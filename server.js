a6cef4c19a930ddbf1107a0161932d905f2cb54e
Steve <zjplove3618739792@gmail.com>
2024-05-28 14:32:05 -0400

    message describing code changes

---
+++ server.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const app = express();
+// 1.
+app.get('/greetings/:name', (req, res) => {
+    res.send(`<h1> What a delight it is to see you once more, ${req.params.name}! </h1>`);
+  });
+// 2.
+
+app.get('/roll/:number', (req, res) => {
+    const number = req.params.number;
+    
+    if ((isNaN(number))) {
+        res.send(`<h1> You must specify a number! </h1>`); 
+    } else if (number >= 0) {
+        res.send(`<h1> You rolled a ${number}! </h1>`); 
+    } 
+});
+
+app.get('/collectibles/:index', (req, res) => {
+     const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+    const index = req.params.index;
+         
+    if (index >= 0 && index < collectibles.length) {
+        const item = collectibles[index];
+        res.send(`<h1>So, you want the ${item.name}? For ${item.price}, it can be yours!</h1>`);
+      } else {
+        res.send('<h1>This item is not yet in stock. Check back soon!</h1>');
+      }
+});
+
+
+  
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  });
+
+
